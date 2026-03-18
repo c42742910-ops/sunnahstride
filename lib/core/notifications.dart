a7bd3c279bed5a83694ca9c2bb12d6998fc3c21c
FixBot <fix@app.com>
2026-03-19 01:19:18 +0200

    fix: notification stub + bare if statements in settings

--- lib/core/notifications.dart
+++ lib/core/notifications.dart
@@ -1,7 +1,9 @@
-// Notifications disabled - flutter_local_notifications removed
+// Notifications stubbed out
 class NotificationService {
   static Future<void> init() async {}
+  static Future<void> requestPermissions() async {}
   static Future<void> scheduleWaterReminder({bool isAr = true}) async {}
   static Future<void> showWorkoutReminder({bool isAr = true}) async {}
   static Future<void> showMealReminder({bool isAr = true}) async {}
+  static Future<void> scheduleFastingReminder({bool isAr = true}) async {}
 }
